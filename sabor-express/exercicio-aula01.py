8bab8f9918dbd2c0ebdd27c78366024f97a2ee41
Maria Isabel <mipt1612@gmail.com>
2024-05-09 20:17:55 -0300

    Adicionar atividades da primeira aula

---
+++ sabor-express/exercicio-aula01.py
@@ -0,0 +1,21 @@
+# Exercícios da primeira aula
+
+# 1 - Imprima a frase: Python na Escola de Programação da Alura.
+
+print('Python na Escola de Programação da Alura.')
+
+# 2 - Imprima a frase: Meu nome é {nome} e tenho {idade} anos em que nome e idade precisam ser valores armazenados em variáveis.
+
+nome = 'Maria Isabel'
+idade = '23'
+print(f'Meu nome é {nome} e tenho {idade} anos.')
+
+# 3 - Imprima a palavra: ‘ALURA’ de modo que cada letra fique em uma linha
+
+print('A','L','U','R','A',sep='\n')
+
+# 4 - Imprima a frase: O valor arredondado de pi é: {pi_arredondado} em que o valor de pi precisa ser armazenado em uma variável e arredondado para apenas duas casas decimais. Para facilitar, utilize: pi = 3.14159
+
+pi = 3.14159
+
+print(f'O valor arredondado de pi é {pi:.2f}')
